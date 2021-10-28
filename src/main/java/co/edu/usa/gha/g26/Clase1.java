a4c2bd78a0d0cb60082083a1579287f256fc8a33
Jhoan Sierra <jhoan.sierra.mt@correo.usa.edu.co>
2021-10-28 18:56:28 -0500

    Primer commit integracion continua

---
+++ src/main/java/co/edu/usa/gha/g26/Clase1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.usa.gha.g26;
+
+/**
+ *
+ * @author Y40-70
+ */
+public class Clase1 {
+    
+    public int suma(int a, int b)
+    {
+        return a + b;
+    }
+}
